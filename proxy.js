b4259e2766a95db91c7c96933ebc77093a63e259
user <user@MacBook-Pro-user.local>
2024-07-10 23:55:10 +0200

    error

--- proxy.js
+++ proxy.js
@@ -8,39 +8,45 @@ app.use(express.static('public')); // Dodanie tej linii, aby serwować pliki sta
 app.use(express.json());
 
 app.use((req, res, next) => {
-  res.header(
-    'Content-Security-Policy',
-    "default-src 'self'; script-src 'self' https://vercel.live"
-  );
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  );
-  next();
+	res.header(
+		'Content-Security-Policy',
+		"default-src 'self'; script-src 'self' https://vercel.live"
+	);
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+	);
+	next();
 });
 
 app.post('/proxy/payment', async (req, res) => {
-  try {
-    const response = await fetch(
-      'https://sandbox.api.imoje.pl/v1/merchant/wh7blvxerihcfs852bsd/payment',
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization:
-            'Bearer 7hw486zjx0v0uby5pc42qvbpxwjhvs4d9msfghc6hlc15r1t1xlcyvevnokc5que',
-        },
-        body: JSON.stringify(req.body),
-      }
-    );
-    const data = await response.json();
-    res.json(data);
-  } catch (error) {
-    res.status(500).json({ error: 'Something went wrong' });
-  }
+	try {
+		const response = await fetch(
+			'https://sandbox.api.imoje.pl/v1/merchant/wh7blvxerihcfs852bsd/payment',
+			{
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+					Authorization:
+						'Bearer 7hw486zjx0v0uby5pc42qvbpxwjhvs4d9msfghc6hlc15r1t1xlcyvevnokc5que',
+				},
+				body: JSON.stringify(req.body),
+			}
+		);
+
+		if (!response.ok) {
+			throw new Error(`HTTP error! status: ${response.status}`);
+		}
+
+		const data = await response.json();
+		res.json(data);
+	} catch (error) {
+		console.error('Error:', error);
+		res.status(500).json({ error: error.message });
+	}
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+	console.log(`Server is running on port ${port}`);
 });
